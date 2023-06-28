e697224678ebf6b44c077cdc8371bbc3af2c0cb0
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-06-28 21:18:21 +0700

    modif crud tabel produk and route crud produk dan kategori produk

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\KategoriProdukController;
 use App\Http\Controllers\PesananController;
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -27,13 +28,31 @@ Route::get('/', function () {
     return view('index');
 });
 
-//Admin
+//Admin produk
 Route::get('/dashboard',[DashboardController::class, 'index']);
 Route::get('/produk',[ProdukController::class, 'index']);
+Route::get('/produk/createproduk', [ProdukController::class, 'create']);
+Route::post('/produk/store', [ProdukController::class, 'store']);
+Route::post('/produk/update', [ProdukController::class, 'update']);
+Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
+Route::get('/produk/viewproduk/{id}', [ProdukController::class, 'show'])->name('show');
+Route::get('/produk/editproduk/{id}', [ProdukController::class, 'edit']);
+
+//Admin kategori_produk
 Route::get('/kproduk',[KategoriProdukController::class, 'index']);
+Route::post('/kproduk/store',[KategoriProdukController::class, 'store']);
+Route::post('/kproduk/update', [KategoriProdukController::class, 'update']);
+Route::get('/kproduk/delete/{id}', [KategoriProdukController::class, 'destroy']);
+Route::get('/kproduk/viewkproduk/{id}', [KategoriProdukController::class, 'show'])->name('show');
+Route::get('/kproduk/createkproduk', [KategoriProdukController::class, 'create'])->name('create');
+Route::get('/kproduk/editkproduk/{id}', [KategoriProdukController::class, 'edit'])->name('edit');
+
+//Admin pesanan
 Route::get('/pesanan',[PesananController::class, 'index']);
+
+//Admin pelanggan
 Route::get('/pelanggan',[PelangganController::class, 'index']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home',[HomeController::class, 'index'])->name('index');
