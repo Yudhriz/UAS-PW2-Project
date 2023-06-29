5ccbb6eafa975d8f9c89f8b32b569b707b5cea70
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-06-29 08:45:55 +0700

    modif index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,9 @@
     <section class="section-slide">
         <div class="wrap-slick1">
             <div class="slick1">
-<<<<<<< HEAD
-                <div class="item-slick1" style="background-image: url({{asset('home/images/slide-01.jpg')}});">
-=======
-            
-                <div class="item-slick1" style="background-image: url({{ asset('home/images/slide-01.jpg') }});">
 
->>>>>>> 577b373cef464702918c1c115b672e024e150f4f
+                <div class="item-slick1" style="background-image: url({{asset('home/images/slide-01.jpg')}});">            
+                <div class="item-slick1" style="background-image: url({{ asset('home/images/slide-01.jpg') }});">
                     <div class="container h-full">
                         <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                             <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
