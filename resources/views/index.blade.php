ebf7c15bbf05c53038353c2052212745a26bc715
Sabrina Ramadhani <sabrinabhinaaa@gmail.com>
2023-06-29 12:35:51 +0700

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <section class="section-slide">
         <div class="wrap-slick1">
             <div class="slick1">
-                <div class="item-slick1" style="background-image: url({{ asset('home/images/slide-01.jpg') }});">
+                <div class="item-slick1" style="background-image: url({{ asset('home/images/slide-1.jpg') }});">
                     <div class="container h-full">
                         <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                             <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
@@ -29,22 +29,22 @@
                     </div>
                 </div>
 
-                <div class="item-slick1" style="background-image: url({{ asset('home/images/slide-02.jpg') }});">
+                <div class="item-slick1" style="background-image: url({{ asset('home/images/slide-2.jpg') }});">
                     <div class="container h-full">
                         <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                             <div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
                                 <span class="ltext-101 cl2 respon2">
-                                    Men New-Season
+                                    
                                 </span>
                             </div>
 
                             <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
                                 <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                                    Jackets & Coats
+                                    
                                 </h2>
                             </div>
 
-                            <div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
+                            <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
                                 <a href="product.html"
                                     class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
                                     Shop Now
@@ -54,12 +54,12 @@
                     </div>
                 </div>
 
-                <div class="item-slick1" style="background-image: url({{ asset('home/images/slide-03.jpg') }});">
+                <div class="item-slick1" style="background-image: url({{ asset('home/images/slide-3.jpg') }});">
                     <div class="container h-full">
                         <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                             <div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
                                 <span class="ltext-101 cl2 respon2">
-                                    Men Collection 2018
+                                    UNMATCHED PERFORMANCE
                                 </span>
                             </div>
 
@@ -90,17 +90,17 @@
                 <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
                     <!-- Block1 -->
                     <div class="block1 wrap-pic-w">
-                        <img src="{{ asset('home/images/banner-01.jpg') }}" alt="IMG-BANNER">
+                        <img src="{{ asset('home/images/benner-1.jpg') }}" alt="IMG-BANNER">
 
                         <a href="product.html"
                             class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child1 flex-col-l">
                                 <span class="block1-name ltext-102 trans-04 p-b-8">
-                                    Women
+                                Electronic
                                 </span>
 
                                 <span class="block1-info stext-102 trans-04">
-                                    Spring 2018
+                                TV
                                 </span>
                             </div>
 
@@ -116,17 +116,17 @@
                 <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
                     <!-- Block1 -->
                     <div class="block1 wrap-pic-w">
-                        <img src="{{ asset('home/images/banner-02.jpg') }}" alt="IMG-BANNER">
+                        <img src="{{ asset('home/images/benner-2.jpg') }}" alt="IMG-BANNER">
 
                         <a href="product.html"
                             class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child1 flex-col-l">
                                 <span class="block1-name ltext-102 trans-04 p-b-8">
-                                    Men
+                                    Laptop
                                 </span>
 
                                 <span class="block1-info stext-102 trans-04">
-                                    Spring 2018
+                                MacBook
                                 </span>
                             </div>
 
@@ -142,17 +142,17 @@
                 <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
                     <!-- Block1 -->
                     <div class="block1 wrap-pic-w">
-                        <img src="{{ asset('home/images/banner-03.jpg') }}" alt="IMG-BANNER">
+                        <img src="{{ asset('home/images/benner-3.jpg') }}" alt="IMG-BANNER">
 
                         <a href="product.html"
                             class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child1 flex-col-l">
                                 <span class="block1-name ltext-102 trans-04 p-b-8">
-                                    Accessories
+                                Kulkas
                                 </span>
 
                                 <span class="block1-info stext-102 trans-04">
-                                    New Trend
+                                Side by side series
                                 </span>
                             </div>
 
@@ -185,7 +185,7 @@
                     </button>
 
                     <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".women">
-                        Women
+                        TV
                     </button>
 
                     <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".men">
@@ -428,7 +428,7 @@
             </div>
 
             <div class="row isotope-grid">
-                <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+                <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item tv">
                     <!-- Block2 -->
                     <div class="block2">
                         <div class="block2-pic hov-img0">
@@ -443,11 +443,11 @@
                         <div class="block2-txt flex-w flex-t p-t-14">
                             <div class="block2-txt-child1 flex-col-l ">
                                 <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                    Esprit Ruffle Shirt
+                                LG OLED evo C2 83 inch 4K Smart TV
                                 </a>
 
                                 <span class="stext-105 cl3">
-                                    $16.64
+                                    Rp. 10.000.000
                                 </span>
                             </div>
 
@@ -478,11 +478,11 @@
                         <div class="block2-txt flex-w flex-t p-t-14">
                             <div class="block2-txt-child1 flex-col-l ">
                                 <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                    Herschel supply
+                                LG LQ57 32 inch Smart TV
                                 </a>
 
                                 <span class="stext-105 cl3">
-                                    $35.31
+                                Rp3.199.000
                                 </span>
                             </div>
 
