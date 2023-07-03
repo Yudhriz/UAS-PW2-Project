abe8bcd900bac8f3019b3698d04f756d274e5bb8
Sabrina Ramadhani <sabrinabhinaaa@gmail.com>
2023-07-03 20:39:32 +0700

    tes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,13 +9,13 @@
                         <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                             <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
                                 <span class="ltext-101 cl2 respon2">
-                                    Women Collection 2018
+                                The most powerful Mac laptop
                                 </span>
                             </div>
 
                             <div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
                                 <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                                    NEW SEASON
+                                COMPARE ALL MAC MODELS
                                 </h2>
                             </div>
 
