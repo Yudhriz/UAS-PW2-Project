ad62f09089cbf664287a515cdd97c58ff664b715
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-07-05 16:51:55 +0700

    add logo admin

--- resources/views/admin/layouts/top.blade.php
+++ resources/views/admin/layouts/top.blade.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
-  <link rel="shortcut icon" type="image/png" href="{{asset('admin/assets/images/logos/favicon.png')}}" />
+  <title>Suntrinic Store</title>
+  <link rel="shortcut icon" type="image/png" href="{{ asset('home/images/icons/logo.png') }}" />
   <link rel="stylesheet" href="{{asset('admin/assets/css/styles.min.css')}}" />
   <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
@@ -22,8 +22,9 @@
       <!-- Sidebar scroll-->
       <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
-          <a href="./index.html" class="text-nowrap logo-img">
-            <img src="{{asset('admin/assets/images/logos/dark-logo.svg')}}" width="180" alt="" />
+          <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+            <img src="{{ asset('home/images/icons/logo.png') }}" width="40" alt="IMG-LOGO" />
+            <h4 style="font-family: 'EB Garamond'; color: black;">Suntronic Store</h4>
           </a>
           <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
             <i class="ti ti-x fs-8"></i>
