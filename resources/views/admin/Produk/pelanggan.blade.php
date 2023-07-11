e0cacba7a4f0190ca347ed07d6ec6e6a82b67e7a
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-07-11 15:31:42 +0700

    modif pelanggan

--- resources/views/admin/Produk/pelanggan.blade.php
+++ resources/views/admin/Produk/pelanggan.blade.php
@@ -14,11 +14,7 @@
         </div>
     </div>
     <div class="card mb-4">
-        <div class="card-header">
-            @if (Auth::user()->role == 'admin')
-                <a class="btn btn-primary" href="{{ url('/admin/pelanggan/createpelanggan') }}">Create Pelanggan</a>
-            @endif
-        </div>
+        <div class="card-header"></div>
         <div class="card-body table-responsive">
             <table id="datatablesSimple">
                 <table class="table table-bordered table-hover">
@@ -28,9 +24,6 @@
                             <th>Nama</th>
                             <th>Alamat</th>
                             <th>Email</th>
-                            @if (Auth::user()->role == 'admin')
-                                <th>Aksi</th>
-                            @endif
                         </tr>
                     </thead>
                     <tbody>
@@ -38,22 +31,9 @@
                         @foreach ($pesanan as $pe)
                             <tr>
                                 <td>{{ $no++ }}</td>
-                                <td>{{ $pe->user->name }}</td>
+                                <td>{{ $pe->user ? $pe->user->name : 'akun telah dihapus' }}</td>
                                 <td>{{ $pe->alamat }}</td>
-                                <td>{{ $pe->user->email }}</td>
-                                @if (Auth::user()->role == 'admin')
-                                    <td>
-                                        <a class="btn btn-primary"
-                                            href="{{ url('/admin/pelanggan/viewpelanggan/' . $pe->id) }}"><i class="fa fa-eye"
-                                                aria-hidden="true"></i></a>
-                                        <a class="btn btn-secondary"
-                                            href="{{ url('/admin/pelanggan/editpelanggan/' . $pe->id) }}"><i
-                                                class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                                        <a class="btn btn-danger" href="{{ url('/admin/pelanggan/delete/' . $pe->id) }}"
-                                            onclick="if(!confirm('Anda Yakin Hapus Data Pelanggan?')) {return false}"><i
-                                                class="fa fa-trash-o" aria-hidden="true"></i></a>
-                                    </td>
-                                @endif
+                                <td>{{ $pe->user ? $pe->user->email : 'akun telah dihapus' }}</td>
                             </tr>
                         @endforeach
                     </tbody>
